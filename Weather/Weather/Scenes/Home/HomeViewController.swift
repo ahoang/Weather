0fbeaf54e6be45dca57969a93bc7f12c0cf128d0
Anthony Hoang <anthony.hoang@slalom.com>
2018-10-13 01:14:58 -0500

    updates to core data

--- Weather/Weather/Scenes/Home/HomeViewController.swift
+++ Weather/Weather/Scenes/Home/HomeViewController.swift
@@ -13,11 +13,12 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var tableView: UITableView!
 
-    private var favorites = [City]()
     private var pins = [String : MKAnnotation]()
+    private var favoritesService = FavoritesService.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.favoritesService.tableView = self.tableView
         self.mapView.delegate = self
         self.tableView.delegate = self
         self.tableView.dataSource = self
@@ -29,17 +30,11 @@ class HomeViewController: UIViewController {
         guard sender.state == .began else { return }
         let location = sender.location(in: mapView)
         let coordinates = mapView.convert(location, toCoordinateFrom: mapView)
-        let pin = MKPointAnnotation()
-        pin.coordinate = coordinates
-        mapView.addAnnotation(pin)
-
 
         WeatherService.getCurrentWeather(lat: coordinates.latitude, long: coordinates.longitude, success: { [weak self] (city) in
             guard let self = self else { return }
             DispatchQueue.main.async {
-                pin.title = city.name
-                self.pins[city.name] = pin
-                self.favorites.append(city)
+                self.favoritesService.insertNewObject(city)
                 self.tableView.reloadData()
             }
 
@@ -56,33 +51,37 @@ class HomeViewController: UIViewController {
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return self.favoritesService.fetchedResultsController.sections?.count ?? 0
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return favorites.count
+        let sectionInfo = self.favoritesService.fetchedResultsController.sections?[section]
+        return sectionInfo?.numberOfObjects ?? 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else {
             fatalError("no cell registered")
         }
-        let city = self.favorites[indexPath.row]
+        let city = self.favoritesService.fetchedResultsController.object(at: indexPath)
         cell.textLabel?.text = city.name
+        let pin = MKPointAnnotation()
+        pin.coordinate = CLLocationCoordinate2D(latitude: city.latitude, longitude: city.longitude)
+        pin.title = city.name
+        self.pins[city.name!] = pin
+        self.mapView.addAnnotation(pin)
 
         return cell
     }
 
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let city = self.favorites[indexPath.row]
-            if let pin = self.pins[city.name] {
+            guard let city = self.favoritesService.fetchedResultsController.object(at: indexPath).name else { return }
+            if let pin = self.pins[city] {
                 self.mapView.removeAnnotation(pin)
-                self.pins[city.name] = nil
+                self.pins[city] = nil
             }
-
-            self.favorites.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            self.favoritesService.deleteObject(indexPath)
         }
     }
 }
--- Weather/Weather/Service/FavoritesService.swift
+++ Weather/Weather/Service/FavoritesService.swift
@@ -8,62 +8,120 @@
 
 import Foundation
 import CoreData
+import UIKit
 
-//class FavoritesService: NSFetchedResultsControllerDelegate {
-//    var managedObjectContext: NSManagedObjectContext? = nil
-//    var _fetchedResultsController: NSFetchedResultsController<Event>? = nil
-//
-//    // MARK: - Fetched results controller
-//
-//    var fetchedResultsController: NSFetchedResultsController<Event> {
-//        if _fetchedResultsController != nil {
-//            return _fetchedResultsController!
-//        }
-//
-//        let fetchRequest: NSFetchRequest<Event> = Event.fetchRequest()
-//
-//        // Set the batch size to a suitable number.
-//        fetchRequest.fetchBatchSize = 20
-//
-//        // Edit the sort key as appropriate.
-//        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
-//
-//        fetchRequest.sortDescriptors = [sortDescriptor]
-//
-//        // Edit the section name key path and cache name if appropriate.
-//        // nil for section name key path means "no sections".
-//        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
-//        aFetchedResultsController.delegate = self
-//        _fetchedResultsController = aFetchedResultsController
-//
-//        do {
-//            try _fetchedResultsController!.performFetch()
-//        } catch {
-//            // Replace this implementation with code to handle the error appropriately.
-//            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//            let nserror = error as NSError
-//            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-//        }
-//
-//        return _fetchedResultsController!
-//    }
-//
-//    @objc
-//    func insertNewObject(_ sender: Any) {
-//        let context = self.fetchedResultsController.managedObjectContext
-//        let newEvent = Event(context: context)
-//
-//        // If appropriate, configure the new managed object.
-//        newEvent.timestamp = Date()
-//
-//        // Save the context.
-//        do {
-//            try context.save()
-//        } catch {
-//            // Replace this implementation with code to handle the error appropriately.
-//            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//            let nserror = error as NSError
-//            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-//        }
-//    }
-//}
+class FavoritesService: NSObject, NSFetchedResultsControllerDelegate {
+    static let shared = FavoritesService()
+    var managedObjectContext: NSManagedObjectContext? = nil
+    var _fetchedResultsController: NSFetchedResultsController<CityEntity>? = nil
+
+    var favorites = [City]()
+    weak var tableView: UITableView?
+
+
+    // MARK: - Fetched results controller
+
+    var fetchedResultsController: NSFetchedResultsController<CityEntity> {
+        if _fetchedResultsController != nil {
+            return _fetchedResultsController!
+        }
+
+        let fetchRequest: NSFetchRequest<CityEntity> = CityEntity.fetchRequest()
+
+        // Set the batch size to a suitable number.
+        fetchRequest.fetchBatchSize = 20
+
+        // Edit the sort key as appropriate.
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+
+        fetchRequest.sortDescriptors = [sortDescriptor]
+
+        // Edit the section name key path and cache name if appropriate.
+        // nil for section name key path means "no sections".
+        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
+        aFetchedResultsController.delegate = self
+        _fetchedResultsController = aFetchedResultsController
+
+        do {
+            try _fetchedResultsController!.performFetch()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+
+        return _fetchedResultsController!
+    }
+
+
+    func insertNewObject(_ sender: City) {
+        let context = self.fetchedResultsController.managedObjectContext
+        let newCity = CityEntity(context: context)
+
+        // If appropriate, configure the new managed object.
+        newCity.name = sender.name
+        newCity.longitude = sender.coordinates.longitude
+        newCity.latitude = sender.coordinates.latitude
+        // Save the context.
+        do {
+            try context.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+
+    func deleteObject(_ indexPath: IndexPath) {
+        let object = self.fetchedResultsController.object(at: indexPath)
+        let context = self.fetchedResultsController.managedObjectContext
+
+        context.delete(object)
+        // Save the context.
+        do {
+            try context.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+
+    // MARK: - Fetched results controller
+
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView?.beginUpdates()
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView?.insertSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .delete:
+            tableView?.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
+        default:
+            return
+        }
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            tableView?.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView?.deleteRows(at: [indexPath!], with: .fade)
+        case .update: fallthrough
+//            configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
+        case .move:
+//            configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
+            tableView?.moveRow(at: indexPath!, to: newIndexPath!)
+        }
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView?.endUpdates()
+    }
+}
